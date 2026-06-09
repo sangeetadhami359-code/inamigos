0404fc315a0c2c20136c6e577b9bda2322394758
Sangeeta <sangeetadhami359@gmail.com>
2026-06-09 15:13:59 +0530

    Clean up temporary automation scripts

--- remove_socials.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('volunteers.html', 'utf8');
-
-// The regex will match <div class="volunteer-socials">...</div> and any nested elements within it
-// until the first closing </div> (assuming no nested divs inside volunteer-socials)
-html = html.replace(/<div class="volunteer-socials">[\s\S]*?<\/div>/g, '');
-
-fs.writeFileSync('volunteers.html', html);
-console.log('Removed socials.');
